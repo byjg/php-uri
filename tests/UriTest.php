d252f70ca1f866e3c1c33d56c284c1d9da481959
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-03-08 22:56:58 -0300

    Removed Backward compatibility PHPUnit

--- tests/UriTest.php
+++ tests/UriTest.php
@@ -2,12 +2,9 @@
 
 namespace ByJG\Util;
 
-// backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-}
+use PHPUnit\Framework\TestCase;
 
-class UriTest extends \PHPUnit\Framework\TestCase
+class UriTest extends TestCase
 {
 
     /**
